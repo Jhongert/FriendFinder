1807342c2e1232925cf0c56a83df9e42ec181d23
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-08-01 13:57:11 -0400

    First commit

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+{
+    "data": [
+        {
+            "name": "John",
+            "photo": "https://www.hoplr.com/Images/defaultuser.jpg",
+            "scores": [
+                1,
+                3,
+                2,
+                3,
+                2,
+                1,
+                4,
+                4,
+                5,
+                3
+            ]
+        },
+        {
+            "name": "nhoj",
+            "photo": "http://norapc.org/wp-content/uploads/2015/07/avatar-blank.png",
+            "scores": [
+                4,
+                1,
+                5,
+                2,
+                4,
+                5,
+                3,
+                4,
+                1,
+                3
+            ]
+        },
+        {
+            "name": "jhongert",
+            "photo": "http://norapc.org/wp-content/uploads/2015/07/avatar-blank.png",
+            "scores": [
+                "5",
+                "2",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "1",
+                "1",
+                "5"
+            ]
+        }
+    ]
+}
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var fs = require('fs');
+
+var filePath = path.join(__dirname, '../data/friends.js');
+
+var friends = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+router.get('/friends', function(req, res){
+	res.json(friends);
+});
+
+router.post('/friends', function(req, res){
+	var scores = req.body.scores;
+	var bestMatch = 0;
+	var bestScore = 99;
+
+	//console.log(req.body);
+
+	for(var i = 0; i < friends.data.length; i++){
+		var difference = 0;
+		
+		 for(var j = 0; j < 10; j++){
+		 	difference += Math.abs(parseInt(friends.data[i].scores[j]) - scores[j]);
+		 	if(difference < bestScore){
+		 		bestScore = difference;
+		 		bestMatch = i;
+		 	}
+		 }
+	}
+	friends.data.push(req.body);
+	fs.writeFile(filePath, JSON.stringify(friends, null, 4), function(err) {
+		if(err) {
+			return console.log(err);
+		}
+	});
+
+	res.send(friends.data[bestMatch]);
+});
+
+module.exports = router;
